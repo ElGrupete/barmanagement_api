319c1841023266ad073aaa1fdf89a21a1ebef455
Eric Farley <efarley@mstech.la>
2020-09-01 23:21:24 -0300

    Refactoring services & routes

--- src/routes/role/role.routes.ts
+++ src/routes/role/role.routes.ts
@@ -1,77 +1,17 @@
+import * as services from './../../services/role.service';
 import express = require("express");
-import { Request, Response } from "express";
-import { DB } from "../../db/db";
 const router = express.Router();
 
 // GET ALL //
-router.get('/', (req: Request, res: Response) => {
-    DB.Models.Role.find({}, (err, roles) => {
-        if (err) {
-            return res.status(500).json({
-                Ok: false,
-                Message: err 
-            });
-        }
-
-        if (roles.length == 0) {
-            return res.status(200).json({
-                Ok: true,
-                Message: 'No se encontraron resultados',
-                Result: roles
-            });
-        }
-
-        res.status(200).json({
-            Ok: true,
-            Result: roles
-        });
-    });
-});
+router.get('/', services.getRoles);
 
 // GET ONE //
-router.get('/:id', (req: Request, res: Response) => {
-
-    let roleId = req.params.id;
-
-    DB.Models.Role.findById(roleId, (err, role) => {
-        if (err) {
-            return res.status(500).json({
-                Ok: false,
-                Message: 'Rol no encontrado' 
-            });
-        }
-
-
-        res.status(200).json({
-            Ok: true,
-            Result: role
-        });
-    });
-});
+router.get('/:id', services.getRoleById);
 
 // POST //
-router.post('/', (req: Request, res: Response) => {
+router.post('/', services.createRole);
 
-    let role = new DB.Models.Role({
-        name: req.body.name,
-        description: req.body.description,
-        admin: req.body.admin
-    });
-
-    role.save( (err, role) => {
-        if (err) {
-            return res.status(400).json({
-                Ok: false,
-                Error: err,
-                Message: 'No se pudo guardar el rol'
-            });
-        }
-
-        res.status(200).json({
-            Ok: true,
-            Result: role
-        });
-    });
-});
+// PUT //
+router.put('/', services.updateRole);
 
 export default router;
--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -1,32 +1,9 @@
+import * as services from './../../services/user.service';
 import express = require("express");
-import { Request, Response } from "express";
-import { DB } from "../../db/db";
-import * as bcryptjs from 'bcryptjs';
 import verifyToken from "../../middlewares/authorization";
 const router = express.Router();
 
 // POST //
-router.post('/', [ verifyToken ], (req: Request, res: Response) => {
-    let user = new DB.Models.User({
-        roleId: req.body.roleId,
-        userName: req.body.userName,
-        password: bcryptjs.hashSync(req.body.password, 10)
-    });
-
-    user.save((err, user) => {
-        if (err) {
-            return res.status(400).json({
-                Ok: false,
-                Error: err,
-                Message: 'No se pudo guardar el usuario'
-            });
-        }
-
-        res.status(200).json({
-            Ok: true,
-            Result: user.userName
-        });
-    });
-});
+router.post('/', [ verifyToken ], services.createUser);
 
 export default router;
