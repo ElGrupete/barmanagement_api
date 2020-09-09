4e5065234a2e41b037c16e80322433e7c52e6d36
Arteks <dolcepablo@gmail.com>
2020-09-08 23:38:36 -0300

    added user actions and routes & start combo module

--- src/services/bar_config/user.service.ts
+++ src/services/bar_config/user.service.ts
@@ -8,7 +8,7 @@ import { DB } from "../../db/db";
 export const createUser = async (req: Request, res: Response) => {
 
     let user = new DB.Models.User({
-        roleId: req.body.roleId,
+        userId: req.body.userId,
         userName: req.body.userName,
         password: bcryptjs.hashSync(req.body.password, 10)
     });
@@ -25,6 +25,81 @@ export const createUser = async (req: Request, res: Response) => {
         res.status(200).json({
             Ok: true,
             Result: { user }
+        });
+    });
+}
+
+export const getUsers = (req: Request, res: Response) => {
+    DB.Models.User.find({}, (err, users) => {
+        if (err) {
+            return res.status(500).json({
+                Ok: false,
+                Message: err 
+            });
+        }
+
+        if (users.length == 0) {
+            return res.status(200).json({
+                Ok: true,
+                Message: 'No se encontraron resultados',
+            });
+        }
+
+        res.status(200).json({
+            Ok: true,
+            Result: {
+                users
+            }
+        });
+    });
+}
+
+export const getUserById = (req: Request, res: Response) => {
+
+    let userId = req.params.id;
+
+    DB.Models.User.findById(userId)
+        .populate('role')
+        .exec((err, user) => {
+        if (err) {
+            return res.status(500).json({
+                Ok: false,
+                Message: 'Usuario no encontrado' 
+            });
+        }
+
+        res.status(200).json({
+            Ok: true,
+            Result: {user, role: user != null ? user.role : null}
+        });
+    });
+}
+
+export const deleteUser = (req: Request, res: Response) => {
+
+    let id = req.body.id;
+
+    DB.Models.User.deleteOne({id}, (err) => {
+        if (err) {
+            return res
+                    .status(500)
+                    .json({
+                        Ok: false,
+                        Error: err,
+                        Message: 'No se pudo actualizar el usuario'
+                    });
+        }
+        if (!id) {
+            return res
+                    .status(404)
+                    .json({
+                        Ok: false,
+                        Message: 'Id no proporcionado'
+                    });
+        }
+        
+        res.json({
+            Ok: true,
         });
     });
 }
